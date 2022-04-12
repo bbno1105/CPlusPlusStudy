4b5ee6d243926bc91b113972e18c0cbc06135840
Name <>
2022-04-12 20:51:43 +0900

    Revert "220412 조건문, 반복문"
    This reverts commit c782d151623137b76c4a7ebce2aa58e636a20765.

--- 220411_Review/220411_Review/220411_Review.cpp
+++ 220411_Review/220411_Review/220411_Review.cpp
@@ -172,104 +172,6 @@
     실습2. 가장 작은 값과 큰 값을 입력 받아서 그 사이의 임의의 값을 출력하는 프로그램을 작성하자.
     실습3. 프로그램을 실행하면 내부적으로 1~9 사이의 숫자가 정답으로 정해진다.
            유저가 1~9 사이의 숫자를 입력하면 맞으면 true, 틀리면 false를 출력한다.
-
-    ====================================================================================
-*/
-
-/*
-    =====================================[ 제어문 ]=====================================
-
-    <제어구조>
-    
-    1. 조건문 : 특정 조건에 따라서 실행문을 실행할지 안 할지 제어 : if / switch
-    2. 반복문 : 특정 조건에 따라서 실행문을 일정 횟수만큼 반복 실행 제어 : for / while
-
-    [ 조건문 ]
-
-    1_1. if 문
-    
-    if (조건식)
-    {
-        실행문1; // 참일 경우
-    }
-    else if (조건식2)
-    {
-        실행문3; // 조건식 거짓, 조건식2 참인 경우
-    }
-    else
-    {
-        실행문2; // 거짓일 경우
-    }
-
-    // 실습1. 
-    정수를 하나 입력 받아서
-    입력 받은 숫자가 10보다 크면 "입력된 숫자가 10보다 큽니다." 출력
-    10 이하 9 초과 이면 "9보다 크빈다." 출력
-    9이 이하 6 초과 이면 "6보다 큽니다."
-    그 이외에는 "6보다 작거나 같습니다."
-
-    
-    1_2. switch 문
-
-    switch (정수형 변수)
-    {
-        case 정수1:
-            실행문1;
-        break;
-
-        case 정수2:
-            실행문2;
-        break;
-
-        default:
-            실행문3; // 해당하는 정수값이 없을 때
-        break;
-    }
-
-    // 실습1.
-    당신의 고향은 어디입니까?
-    1. 서울 2. 양평 3. 광주 4. 대전 5. 제주도
-    _입력을 받음
-
-    안녕하세요. 당신의 고향은 서울이군요.
-
-
-
-    [ 반복문 ]
-
-    2_1. while
-
-    while (조건식)
-    {
-        실행문; // 조건식이 참일 경우 반복해서 실행
-        조건식을 바꾸는 실행문; // while문을 중단시키는 첫번째 방법
-
-        if (조건식)
-        {
-            break; // while문을 중단시키는 두번째 방법
-        }
-    }
-
-    do-while
-    do // 최초 1회 실행은 무조건
-    {
-        실행문;
-     }
-    while (조건식); // 조건식을 판단하고 do를 다시 실행할지 판단
-
-
-    2_2. for
-
-    for ( (1)초기식; (2)조건식; (3)증감식)
-    {
-        (4)실행문;
-    }
-    
-    진행 순서 : (1) -> (2) -> (4) -> (3) -> (2) -> (4) -> (3) -> 반복
-
-
-
-    ====================================================================================
 */
 
 #include <iostream> // iostream의 라이브러리를 쓰겠다!
@@ -278,325 +180,55 @@
 using namespace std;
 
 
-
 int main()
 {
+    //// 실습 0
+    //srand(time(NULL));
+    //cout << "rand()의 크기 : " << sizeof(rand()) << endl;
+
+    //cout << endl;
+
+    //// 실습 1
+    //cout << "[실습 1]" << endl;
+    //cout << "답 : " << rand() % 5 + 3 << endl;
+
+    //cout << endl;
+
+
+    //// 실습 2
+    //cout << "[실습 2]" << endl;
+    //
+    //int value1; // 첫번째 값
+    //int value2; // 두번째 값
+
+    //cout << "첫번째 값을 입력해 주세요 : ";
+    //cin >> value1;
+    //cout << "두번째 값을 입력해 주세요 : ";
+    //cin >> value2;
+    //
+    //(value2 <= value1) ? // 최소, 최대 값이 
+    //    cout << "답 : " << rand() % (value1 - value2 + 1) + value2 << endl :
+    //    cout << "답 : " << rand() % (value2 - value1 + 1) + value1 << endl;
+
+    //cout << endl;
+
+
+    //// 실습 3
+    //cout << "[실습 3]" << endl;
+    //int correctAnswer = rand() % 9 + 1;
+
+    //int yourAnswer;
+    //cout << "1~9 사이의 값을 입력해 주세요 : ";
+    //cin >> yourAnswer;
+    //(1 <= yourAnswer && yourAnswer <= 9) ? // 정확한 수를 입력 했는가?
+    //    ((correctAnswer == yourAnswer) ?   // 선택한 값이 랜덤한 숫자와 같은가?
+    //        cout << "정답입니다!!" << endl : 
+    //        cout << "당신은 틀렸습니다. 정답은 " << correctAnswer << "입니다." << endl) : 
+    //    cout << "범위를 벗어난 수를 입력하였습니다." << endl;
+
+
+
     srand(time(NULL));
-
-#pragma region for문실습
-
-    // 10부터 1까지 차례대로 출력되는 코드를 작성해 보자 10 9 8 7
-    for (int i = 10; i > 0; i--)
-    {
-        cout << "1~10 : " << i << endl;
-    }
-
-    // 100미만의 3의 배수를 차례대로 출력되는 코드를 작성해 보자 3 6 9 12
-    for (int i = 1; 3 * i < 100; i++)
-    {
-        cout << "3의 배수 : " << 3 * i << endl;
-    }
-    // 이런 방법도 있다.
-    //for (int i = 3; i < 100; i += 3)
-    //{
-    //    cout << "3의 배수 : " << i << endl;
-    //}
-
-    // 1000미만의 2의 승수를 차례대로 출력되는 코드를 작성해보자. 1 2 4 8 16
-    // pow(2,i)
-    for (int i = 0; pow(2,i) < 1000; i++)
-    {
-        cout << "2의 승수 : " << pow(2, i) << endl;
-    }
-    // pow()를 사용하지 않아도 이렇게 간단하게도 할 수 있다.
-    // for (int i = 1; i < 1000; i *=2)
-    // {
-    //     cout << "2의 승수 : " << i << endl;
-    // }
-
-    // [실습4] 구구단을 출력해 보자
-    cout << "\n[실습 4-1]" << endl;
-    for (int i = 1; i < 10; i++)
-    {
-        cout << 2 << " * " << i << " = " << (2 * i) << endl;
-    }
-
-    cout << "\n[실습 4-2]" << endl;
-    for (int i = 2; i < 10; i++)
-    {
-        cout << i << "단" << endl;
-        for (int j = 1; j < 10; j++)
-        {
-            cout << i << " * " << j << " = " << (i*j) << endl;
-        }
-        cout << endl;
-    }
-
-    cout << "\n[실습 4-3]" << endl;
-    for (int i = 2; i < 10; i += 3) // i : 단
-    {
-        for (int j = 0; j < 10; j++) // j : 곱하는 값
-        {
-            // 그려주는 곳
-            for (int k = 0; k < 3; k++) // k : 한 줄에 3개씩 그려야 하니까 묶어줄게
-            {
-                if (j == 0 && (i + k) < 10) // 0줄이면 단을 써줄거야!! 근데 10단은 없어~
-                {
-                    cout << "[ " << i + k << " 단 ]\t";
-                }
-                else // 0줄이 아닐때만 들어와라 
-                {
-                    if (i + k < 10) // 10단을 넘지 않도록 
-                    {
-                        printf("%d * %d = %2d\t", i + k, j, (i + k) * j);
-                        
-                        // 이렇게 해도 됩니다.
-                        // cout << i + k << " * " << j << " = " << ((i+k) * j) << "\t";
-                    }
-                }
-            }
-            cout << endl; // 다음 줄로
-        }
-        cout << endl; // i + k 단을 다 그리고 다음 줄로
-    }
-
-    /*
-    2단 3단 4단
-    5단 6단 7단
-    8단 9단
-    */
-
-#pragma endregion
-
-#pragma region while문실습2
-    int correctQuizAnswer = rand() % 9 + 1;
-    // 난수실습 3을 while문을 사용하여 정답을 맞출 때 까지 반복되는 프로그램으로 수정하기
-    int yourQuizAnswer;
-    cout << "미리보는 정답 : " << correctQuizAnswer << endl;
-
-    while (true)
-    {
-        cout << "1~9 사이의 값을 입력해 주세요 : ";
-        cin >> yourQuizAnswer;
-
-        if (correctQuizAnswer == yourQuizAnswer)
-        {
-            cout << "정답입니다!!" << endl;
-            break;
-        }
-        else if (yourQuizAnswer < 1 || yourQuizAnswer > 9)
-        {
-            cout << "범위를 벗어난 수를 입력하였습니다. 다시 입력해 주세요." << endl;
-        }
-        else
-        {
-            cout << "당신은 틀렸습니다. 다시 도전해 보세요. " << endl;
-        }
-    }
-
-    int correctQuizAnswer2 = rand() % 9 + 1;
-    cout << "미리보는 정답2 : " << correctQuizAnswer2 << endl;
-
-    int yourQuizAnswer2 = -1; // 초기식
-
-    while (yourQuizAnswer2 != correctQuizAnswer2)
-    {
-        cout << "1~9 사이의 값을 입력해 주세요2 : ";
-        cin >> yourQuizAnswer2;
-
-        if (correctQuizAnswer2 == yourQuizAnswer2)
-        {
-            cout << "정답입니다!!" << endl;
-        }
-        else if (yourQuizAnswer2 < 1 || yourQuizAnswer2 > 9)
-        {
-            cout << "범위를 벗어난 수를 입력하였습니다. 다시 입력해 주세요." << endl;
-        }
-        else
-        {
-            cout << "당신은 틀렸습니다. 다시 도전해 보세요. " << endl;
-        }
-    }
-    
-
-#pragma endregion
-
-#pragma region while문실습
-    // while 1
-    int count;
-    cin >> count;
-
-    while (count > 0)
-    {
-        cout << "HelloWorld" << endl;
-        count--;
-
-    }
-
-    // while 2
-    int count2;
-    cin >> count2;
-    while (true)
-    {
-        cout << "HelloWorld if" << endl;
-        count2--;
-
-        if(count2 <= 0)
-        {
-            break;
-        }
-    }
-
-    // do_while
-    int count3;
-    cin >> count3;
-    do
-    {
-        cout << "HelloWorld do_while" << endl;
-        count3--;
-
-    } while (count3 > 0);
-#pragma endregion
-
-#pragma region switch문실습
-    int selectNumber;
-
-    cout << "당신의 고향은 어디입니까?\n 1.서울 2.양평 3.광주 4.대전 5.제주도" << endl;
-    cin >> selectNumber;
-
-    switch (selectNumber) 
-    {
-        case 1: // 서울
-            cout << "안녕하세요. 당신의 고향은 서울이군요." << endl;
-            break;
-
-        case 2: // 양평
-            cout << "안녕하세요. 당신의 고향은 양평이군요." << endl;
-            break;
-
-        case 3: // 광주
-            cout << "안녕하셨지라. 당신의 고향은 광주이군요." << endl;
-            break;
-
-        case 4: // 대전
-            cout << "안녕하세유. 당신의 고향은 대전이군요." << endl;
-            break;
-
-        case 5: // 제주도
-            cout << "안녕하수꽝. 당신의 고향은 제주도이군요." << endl;
-            break;
-
-        default:
-            cout << "5개 중에 선택해 주세요." << endl;
-            break;
-    }
-
-    // switch문 -> if문 으로 표현하기
-    if (selectNumber == 1) // 서울
-    {
-        cout << "안녕하세요. 당신의 고향은 서울이군요." << endl;
-    }
-    else if (selectNumber == 2) // 양평
-    {
-        cout << "안녕하세요. 당신의 고향은 양평이군요." << endl;
-    }
-    else if (selectNumber == 3) // 광주
-    {
-        cout << "안녕하셨지라. 당신의 고향은 광주이군요." << endl;
-    }
-    else if (selectNumber == 4) // 대전
-    {
-        cout << "안녕하세유. 당신의 고향은 대전이군요." << endl;
-    }
-    else if (selectNumber == 5) // 제주도
-    {
-        cout << "안녕하수꽝. 당신의 고향은 제주도이군요." << endl;
-    }
-    else
-    {
-        cout << "5개 중에 선택해 주세요." << endl;
-    }
-    cout << endl;
-#pragma endregion
-
-#pragma region if문실습
-    int inputNumber;
-
-    cout << "아무 값이나 입력해 주세요 : " << endl;
-    cin >> inputNumber;
-
-    if (inputNumber > 10) 
-    {
-        cout << "입력된 숫자가 10보다 큽니다." << endl;
-    }
-    else if (inputNumber > 9) 
-    {
-        cout << "9보다 큽니다." << endl;
-    }
-    else if (inputNumber > 6)
-    {
-        cout << "6보다 큽니다." << endl;
-    }
-    else
-    {
-        cout << "6보다 작거나 같습니다" << endl << endl;
-    }
-#pragma endregion
-    
-#pragma region 난수실습0
-    // 실습 0
-    //출력
-    cout << "rand()의 크기 : " << sizeof(rand()) << endl;
-    cout << endl;
-#pragma endregion
-
-#pragma region 난수실습1
-    // 실습 1
-    //출력
-    cout << "[실습 1]" << endl;
-    cout << "답 : " << rand() % 5 + 3 << endl;
-    cout << endl;
-#pragma endregion
-
-#pragma region 난수실습2
-    // 실습 2
-    cout << "[실습 2]" << endl;
-    
-    int value1; // 첫번째 값
-    int value2; // 두번째 값
-
-    //입력
-    cout << "첫번째 값을 입력해 주세요 : ";
-    cin >> value1;
-    cout << "두번째 값을 입력해 주세요 : ";
-    cin >> value2;
-    
-    //출력
-    (value2 <= value1) ? // 최소, 최대 값이 
-        cout << "답 : " << rand() % (value1 - value2 + 1) + value2 << endl :
-        cout << "답 : " << rand() % (value2 - value1 + 1) + value1 << endl;
-
-    cout << endl;
-#pragma endregion
-
-#pragma region 난수실습3
-    // 실습 3
-    cout << "[실습 3]" << endl;
-    int correctAnswer = rand() % 9 + 1;
-
-    int yourAnswer;
-    cout << "1~9 사이의 값을 입력해 주세요 : ";
-    cin >> yourAnswer;
-    (1 <= yourAnswer && yourAnswer <= 9) ? // 정확한 수를 입력 했는가?
-        ((correctAnswer == yourAnswer) ?   // 선택한 값이 랜덤한 숫자와 같은가?
-            cout << "정답입니다!!" << endl : 
-            cout << "당신은 틀렸습니다. 정답은 " << correctAnswer << "입니다." << endl) : 
-        cout << "범위를 벗어난 수를 입력하였습니다." << endl;
-#pragma endregion
-
-
-
-    // srand(time(NULL));
 
     cout << rand() << endl;  // 41이 나옴
 
